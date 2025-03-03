07bb4ccc33309945db393042f4d2082ea42401ed
Your Name <leonardo.iwama25@outlook.com>
2025-03-03 00:30:00 -0400

    fix: ajustes acesso a rotas

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,18 +1,23 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore();
-  if (authStore.getAccessToken() === "") {
-    const tokenLocalStorage = computed(() =>
-      JSON.parse(window?.localStorage?.getItem("token") || "{}")
-    );
+  const authToken = authStore.getAccessToken();
+  const tokenLocalStorage = JSON.parse(
+    window?.localStorage?.getItem("token") || "{}"
+  );
 
-    if (tokenLocalStorage.value?.email) {
-      authStore.setEmail(tokenLocalStorage.value.email);
+  if (!authToken && to.path !== "/") {
+    if (tokenLocalStorage?.email) {
+      authStore.setEmail(tokenLocalStorage.email);
     }
 
-    if (tokenLocalStorage.value?.token?.access_token) {
-      authStore.setToken(tokenLocalStorage.value.token);
+    if (tokenLocalStorage?.token?.access_token) {
+      authStore.setToken(tokenLocalStorage.token);
     } else {
       authStore.logout();
     }
+  }
+
+  if (to.path === "/" && (authToken || tokenLocalStorage?.token)) {
+    return navigateTo("/usuarios");
   }
 });
