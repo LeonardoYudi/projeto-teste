8b21c5dce47d96f45224c361f626cebab1afa189
Your Name <leonardo.iwama25@outlook.com>
2025-03-03 18:47:50 -0400

    feat: melhoria middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,23 +1,21 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore();
-  const authToken = authStore.getAccessToken();
+  let authToken = authStore.getAccessToken();
+
   const tokenLocalStorage = JSON.parse(
     window?.localStorage?.getItem("token") || "{}"
   );
 
-  if (!authToken && to.path !== "/") {
-    if (tokenLocalStorage?.email) {
-      authStore.setEmail(tokenLocalStorage.email);
-    }
-
-    if (tokenLocalStorage?.token?.access_token) {
-      authStore.setToken(tokenLocalStorage.token);
-    } else {
-      authStore.logout();
-    }
+  if (!authToken && tokenLocalStorage?.token?.access_token) {
+    authStore.setToken(tokenLocalStorage.token);
+    authToken = authStore.getAccessToken();
   }
 
-  if (to.path === "/" && (authToken || tokenLocalStorage?.token)) {
+  if (!authToken && to.path !== "/") {
+    return navigateTo("/");
+  }
+
+  if (to.path === "/" && authToken) {
     return navigateTo("/usuarios");
   }
 });
